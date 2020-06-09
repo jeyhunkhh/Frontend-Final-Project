cabc264eae9c40fb94563fe9a8f9dd2630a79037
jeyhunkhh <jeyhunkhh@code.edu.az>
2020-06-10 03:00:32 +0400

    countTo.js and appeare.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,75 +1,104 @@
 $(document).ready(function () {
+  $(".img-loader").fadeOut("hide");
+
   // Search //
-  $(".search-icon").click(function (e) {
-    e.preventDefault();
-    $("form").slideToggle("show");
-  });
+  if ($(".search-icon").length) {
+    $(".search-icon").click(function (e) {
+      e.preventDefault();
+      $("form").slideToggle("show");
+    });
+  }
   // ----- //
 
   // Side menu //
-  $(".side-menu-icon").click(function (e) {
-    e.preventDefault();
-    $(".side-menu").addClass("open");
-  });
+  if ($(".side-menu-icon").length) {
+    $(".side-menu-icon").click(function (e) {
+      e.preventDefault();
+      $(".side-menu").addClass("open");
+    });
+  }
 
-  $(".close-side").click(function (e) {
-    e.preventDefault();
-    $(".side-menu").removeClass("open");
-  });
+  if ($(".close-side").length) {
+    $(".close-side").click(function (e) {
+      e.preventDefault();
+      $(".side-menu").removeClass("open");
+    });
+  }
   // ----- //
 
   // Owl carousel 2//
-  $(".slider .owl-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    autoplay: true,
-    autoplayTimeout: 4000,
-    autoplayHoverPause: false,
-    dotsEach: 1,
-    responsive: {
-      0: {
-        items: 1,
+  if ($(".slider .owl-carousel").length) {
+    $(".slider .owl-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: false,
+      dotsEach: 1,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        1024: {
+          items: 1.5,
+        },
       },
-      1024: {
-        items: 1.5,
-      },
-    },
-  });
+    });
+  }
+  // ----- //
+
+  // Tabs header
+  if ($(".tabs-header").length) {
+    $(".tabs-header a").click(function (e) {
+      e.preventDefault();
+      let href = $(this).attr("href");
+
+      $(this).parent().siblings().children().removeClass("active");
+      $(this).addClass("active");
+
+      if ($(href).hasClass("animate__fadeIn")) {
+        return;
+      }
+
+      $(href).siblings().removeClass("animate__fadeIn").addClass("hide");
+      $(href).removeClass("hide").addClass("animate__fadeIn");
+    });
+  }
   // ----- //
   
-  $(".tabs-header a").click(function(e){
-    e.preventDefault();
-    let href= $(this).attr("href");
-
-    $(this).parent().siblings().children().removeClass("active");
-    $(this).addClass("active");
-
-    if($(href).hasClass("animate__fadeIn")){
-      return;
-    };
-
-    $(href).siblings().removeClass("animate__fadeIn").addClass("hide");
-    $(href).removeClass("hide").addClass("animate__fadeIn");
-        
-  })
-
+  $('.fun-factor-content').appear(function() {
+      $('.timer').countTo();
+  }, {
+      accY: -100
+  });
+  
   // wow.js //
   function wowAnimation() {
-    wow = new WOW(
-      {
-      boxClass:     'wow',      // default
-      animateClass: 'animated', // default
-      offset:       0,          // default
-      mobile:       true,       // default
-      live:         true        // default
-    }
-    )
+    wow = new WOW({
+      boxClass: "wow", // default
+      animateClass: "animated", // default
+      offset: 0, // default
+      mobile: true, // default
+      live: true, // default
+    });
     wow.init();
-  };
+  }
   // ----- //
-  
-  $(window).on("load", function(){
+
+  $(window).on("load", function () {
     wowAnimation();
-  })
+  });
+
+  $(window).scroll(function (e) {
+    if ($(window).scrollTop() > 100) {
+      $("#navbar").addClass("fixed fadeInDown");
+    } else {
+      $("#navbar").removeClass("fixed fadeInDown");
+    }
+  });
+});
+
+$(window).on("load", function () {
+  $(".img-loader").fadeOut("slow");
 });
